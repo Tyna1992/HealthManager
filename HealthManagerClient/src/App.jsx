bc2c933164abe9e3e46989a74855e8ab540ceea7
Emil9411 <almadi.emil94@gmail.com>
2024-02-15 09:37:42 +0100

    The basic look of the site is created

--- HealthManagerClient/src/App.jsx
+++ HealthManagerClient/src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src="/heartlogo.png" alt="logo" width="70%" height="auto" />
+        <h1>Welcome to the Health Manager App!</h1>
+        <div id="toolbar">
+          <button>Home</button>
+          <button>Registration</button>
+          <button>Login</button>
+          <button>Search for activities</button>
+          <button>Search for nutrients</button>
+          <button>Search for recipes</button>
+          <button>Search for drinks(?)</button>
+        </div>
+      </header>
+    </div>
   )
 }
 
