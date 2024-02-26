f54b03af5652076d5942fb783071e9b770bfd16a
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-02-26 14:21:24 +0100

    Repository layer added for user management
    Co-authored-by: Emil9411 <Emil9411@users.noreply.github.com>
    Co-authored-by: szoszo23 <szoszo23@users.noreply.github.com>

---
+++ HealthManagerServer/HealthManagerServer/Service/IUserRepository.cs
@@ -0,0 +1,13 @@
+
+
+using HealthManagerServer.Model;
+
+public interface IUserRepository
+{
+    IEnumerable<User> GetAll();
+    User? GetByUserName(string userName);
+    void AddUser(User user);
+    void DeleteUser(User user);
+    void UpdateUser(User user);
+    
+}
--- HealthManagerServer/HealthManagerServer/Service/UserRepository.cs
+++ HealthManagerServer/HealthManagerServer/Service/UserRepository.cs
@@ -2,83 +2,46 @@
 using HealthManagerServer.Model;
 
 namespace HealthManagerServer.Service;
+
+using System.Collections.Generic;
+using HealthManagerServer.Data;
 using Microsoft.Data.SqlClient;
 
 
-public class UserRepository
+public class UserRepository : IUserRepository
 {
-    private readonly string _connectionString;
+    private readonly UserContext _context;
     
-    public UserRepository()
+    public UserRepository(UserContext context)
     {
-        //DotEnv.Load(new DotEnvOptions(envFilePaths: new[] {"../.env"}));
-        //var envVars = DotEnv.Read();
-        //Console.WriteLine(envVars["DATABASES__SQLSERVER__CONNECTIONSTRING"]);
-        //_connectionString = envVars["DATABASES__SQLSERVER__CONNECTIONSTRING"];
-        _connectionString = "Server=tcp:mysqlserverhun.database.windows.net,1433;Initial Catalog=SQLDatabase;Persist Security Info=False;User ID=azureuser;Password=121212EmQ1994!%;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
-    }
-    
-    private SqlConnection GetConnection()
-    {
-        var connection = new SqlConnection(_connectionString);
-        connection.Open();
-        return connection;
-    }
-    
-    private void ExecuteNonQuery(string query)
-    {
-        using var connection = GetConnection();
-        using var command = new SqlCommand(query, connection);
-        command.ExecuteNonQuery();
-    }
-    
-    private static SqlCommand CreateCommand(SqlConnection connection, string query)
-    {
-        return new SqlCommand
-        {
-            CommandText = query,
-            Connection = connection,
-        };
+        _context = context;
     }
 
-    public void CreateUserTable()
-    {
-        var query = @"IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Users')
-              BEGIN
-                  CREATE TABLE Users (Id UNIQUEIDENTIFIER PRIMARY KEY, 
-                                      UserName NVARCHAR(255), 
-                                      Email NVARCHAR(255), 
-                                      Password NVARCHAR(255), 
-                                      Weight FLOAT,
-                                      Gender NVARCHAR(10))
-              END";
-        ExecuteNonQuery(query);
-        
-    }
-    
     public void AddUser(User user)
     {
-        var query = @"INSERT INTO Users (Id, UserName, Email, Password, Weight, Gender) 
-                  VALUES (@Id, @UserName, @Email, @Password, @Weight, @Gender)";
-        var hashedPassword = BCrypt.Net.BCrypt.HashPassword(user.Password);
-        using (var connection = new SqlConnection(_connectionString))
-        {
-            connection.Open();
-            using (var command = new SqlCommand(query, connection))
-            {
-                command.Parameters.AddWithValue("@Id", user.Id);
-                command.Parameters.AddWithValue("@UserName", user.UserName);
-                command.Parameters.AddWithValue("@Email", user.Email);
-                command.Parameters.AddWithValue("@Password", hashedPassword);
-                command.Parameters.AddWithValue("@Weight", user.Weight);
-               
-                command.Parameters.AddWithValue("@Gender", user.Gender);
-            
-                command.ExecuteNonQuery();
-            }
-        }
+        _context.Users.Add(user);
+        _context.SaveChanges();
     }
-    
-    
+
+    public void DeleteUser(User user)
+    {
+        _context.Remove(user);
+        _context.SaveChanges();
+    }
+
+    public IEnumerable<User> GetAll()
+    {
+        return _context.Users.ToList();
+    }
+
+    public User? GetByUserName(string userName)
+    {
+        return _context.Users.FirstOrDefault(user => user.UserName == userName);
+    }
+
+    public void UpdateUser(User user)
+    {
+        _context.Users.Update(user);    
+        _context.SaveChanges();
+    }
 }
