77a2355c2af313a3134917d501bf2199c906a9f3
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-02-28 12:15:45 +0100

    Repository layer added for handling activities

---
+++ HealthManagerServer/HealthManagerServer/Service/ActivityRepository.cs
@@ -0,0 +1,42 @@
+﻿using HealthManagerServer.Data;
+using HealthManagerServer.Model;
+
+namespace HealthManagerServer.Service;
+
+public class ActivityRepository : IActivityRepository
+{
+    private readonly DataBaseContext _context;
+    
+    public ActivityRepository(DataBaseContext context)
+    {
+        _context = context;
+    }
+    
+    public IEnumerable<Activity> GetAll()
+    {
+        return _context.Activities.ToList();
+    }
+
+    public Activity? GetByActivityName(string name)
+    {
+        return _context.Activities.FirstOrDefault(a => a.Name == name);
+    }
+
+    public void AddActivity(Activity activity)
+    {
+        _context.Activities.Add(activity);
+        _context.SaveChanges();
+    }
+
+    public void DeleteActivity(Activity activity)
+    {
+        _context.Activities.Remove(activity);
+        _context.SaveChanges();
+    }
+
+    public void UpdateActivity(Activity activity)
+    {
+        _context.Activities.Update(activity);
+        _context.SaveChanges();
+    }
+}
---
+++ HealthManagerServer/HealthManagerServer/Service/IActivityRepository.cs
@@ -0,0 +1,13 @@
+﻿using HealthManagerServer.Model;
+
+namespace HealthManagerServer.Service;
+
+public interface IActivityRepository
+{
+    IEnumerable<Activity> GetAll();
+    Activity? GetByActivityName(string name);
+    void AddActivity(Activity activity);
+    void DeleteActivity(Activity activity);
+    void UpdateActivity(Activity activity);
+    
+}
