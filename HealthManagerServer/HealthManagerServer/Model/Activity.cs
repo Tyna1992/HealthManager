d20a4791b4ebd088cc7a49b543e99365f7863a02
Emil9411 <almadi.emil94@gmail.com>
2024-02-29 10:38:46 +0100

    Activity model modified for better data representation.
    Co-authored-by: Takács Tímea Erika <Tyna1992@users.noreply.github.com>
    Co-authored-by: szoszo23 <szoszo23@users.noreply.github.com>

--- HealthManagerServer/HealthManagerServer/Model/Activity.cs
+++ HealthManagerServer/HealthManagerServer/Model/Activity.cs
@@ -4,8 +4,8 @@ public class Activity
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public double CaloriesBurnedPerHour { get; set; }
+    public double Calories_per_hour { get; set; }
     public int Weight { get; set; }
-    public int Duration { get; set; }
-    public double TotalCaloriesBurned { get; set; }
+    public int Duration_minutes { get; set; }
+    public double Total_calories { get; set; }
 }
