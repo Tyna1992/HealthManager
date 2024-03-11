3b7812a3637f349990bd70d60e555d794e4ba80f
Emil9411 <almadi.emil94@gmail.com>
2024-03-11 15:29:01 +0100

    Updated the UserContext.cs with custom user model.
    Co-authored-by: Takács Tímea Erika <Tyna1992@users.noreply.github.com>
    Co-authored-by: szoszo23 <szoszo23@users.noreply.github.com>

--- HealthManagerServer/HealthManagerServer/Data/UserContext.cs
+++ HealthManagerServer/HealthManagerServer/Data/UserContext.cs
@@ -1,21 +1,25 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using HealthManagerServer.Model;
 
-public class UserContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+namespace HealthManagerServer.Data
 {
-    public UserContext (DbContextOptions<UserContext> options)
-        : base(options)
+    public class UserContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
     {
-    }
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer("Server=tcp:mysqlserverhun.database.windows.net,1433;Initial Catalog=Base;Persist Security Info=False;User ID=azureuser;Password=121212EmQ1994!%;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-    }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=tcp:mysqlserverhun.database.windows.net,1433;Initial Catalog=Base;Persist Security Info=False;User ID=azureuser;Password=121212EmQ1994!%;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
 
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
 }
---
+++ HealthManagerServer/HealthManagerServer/Migrations/User/20240311135826_AddNewColoumns.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthManagerServer.Migrations.User
+{
+    /// <inheritdoc />
+    public partial class AddNewColoumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Weight",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Weight",
+                table: "AspNetUsers");
+        }
+    }
+}
