6bb81bf136529d40624205671cb3f0b484b7095c
Emil9411 <almadi.emil94@gmail.com>
2024-03-12 15:10:06 +0100

    New table design implemented.
    Co-authored-by: Takács Tímea Erika <Tyna1992@users.noreply.github.com>
    Co-authored-by: szoszo23 <szoszo23@users.noreply.github.com>

--- HealthManagerClient/src/Pages/Meals.jsx
+++ HealthManagerClient/src/Pages/Meals.jsx
@@ -1,83 +1,98 @@
 ﻿import { useState } from "react";
 import "../index.css";
-import { capitalizeWords } from "../../utilities/utils";
-
+import TableComponent from "../Components/TableComponent";
 
 function Meals(props) {
+  const [meal, setMeal] = useState("");
+  const [amount, setAmount] = useState("");
+  const [nutritionalValue, setNutritionalValue] = useState([]);
+  const [hideResult, setHideResult] = useState(true);
 
-    const [meal, setMeal] = useState("");
-    const [amount, setAmount] = useState("");
-    const [nutritionalValue, setNutritionalValue] = useState({});
-    const [hideResult, setHideResult] = useState(true);
-
-    const fetchNutritionalValue = async () => {
-        
-        if(amount === "" || meal === ""){
-            alert("Please fill out both fields.")
-            return;
-        }
-        try {
-            const response = await fetch(`/api/meal/${amount}/${meal}`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
-            if(!response.ok){
-                alert("Cannot find the nutritional value for the meal. Please try again.")
-                throw new Error("Fetch failed!")
-            }
-            const data = await response.json();
-            if(data.length === 0){
-                alert("Cannot find the nutritional value for the meal. Please try again.")
-                throw new Error("Cannot find the nutritional value for the meal. Please try again.") 
-            }
-            setNutritionalValue(data);
-            console.log(data);
-            setHideResult(false);
-        } catch (error) {
-            console.error(error);
-        }
+  const fetchNutritionalValue = async () => {
+    if (amount === "" || meal === "") {
+      alert("Please fill out both fields.");
+      return;
     }
-
-    const handleClear = () =>{
-        setMeal("");
-        setAmount("");
+    try {
+      const response = await fetch(`/api/meal/${amount}/${meal}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        alert("Cannot find the nutritional value for the meal. Please try again.");
+        throw new Error("Fetch failed!");
+      }
+      const data = await response.json();
+      if (data.length === 0) {
+        alert("Cannot find the nutritional value for the meal. Please try again.");
+        throw new Error("Cannot find the nutritional value for the meal. Please try again.");
+      }
+      setNutritionalValue(data);
+      console.log(data);
+      // console.log(Object.keys(data[0]));
+      setHideResult(false);
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    const handleMealChange = (event) => {
-        setMeal(event.target.value);
-    };
+  const handleClear = () => {
+    setMeal("");
+    setAmount("");
+  };
 
-    const handleAmountChange = (event) => {
-        setAmount(event.target.value);
-    };
-    
-    return (
-        <div>
-            <label>Meal name:</label>
-            <br></br>
-            <input required type="text" name="meal" value={meal} placeholder="Name of the meal" onChange={handleMealChange}></input>
-            <br></br>
-            <label>Serving size:</label>
-            <br></br>
-            <input required type="text" name= "amount" value={amount} placeholder="In gramms" onChange={handleAmountChange}></input>
-            <br></br>
-            <button onClick={() => fetchNutritionalValue()}>Show nutritional value</button>
-            <button onClick={()=> {setHideResult(true);handleClear()}}>Clear</button>
+  const handleMealChange = (event) => {
+    setMeal(event.target.value);
+  };
 
-            {<div hidden={hideResult} id="result">
-                <h2>Nutritional value of: {capitalizeWords(meal)}</h2>
-                <p>Serving size: {nutritionalValue.serving_size_g} g</p>
-                <p>Calories: {nutritionalValue.calories} cal</p>
-                <p>Protein: {nutritionalValue.protein_g} g</p>
-                <p>Fat: {nutritionalValue.fat_total_g} g</p>
-                <p>Carbohydrates: {nutritionalValue.carbohydrates_total_g} g</p>
-                <p>Sugar: {nutritionalValue.sugar_g} g</p>
-                <p>Sodium: {nutritionalValue.sodium_mg} mg</p>
-            </div> }
-        </div>
-    )
+  const handleAmountChange = (event) => {
+    setAmount(event.target.value);
+  };
+
+  return (
+    <div>
+      <label>Meal name:</label>
+      <br></br>
+      <input
+        required
+        type="text"
+        name="meal"
+        value={meal}
+        placeholder="Name of the meal"
+        onChange={handleMealChange}
+      ></input>
+      <br></br>
+      <label>Serving size:</label>
+      <br></br>
+      <input
+        required
+        type="text"
+        name="amount"
+        value={amount}
+        placeholder="In gramms"
+        onChange={handleAmountChange}
+      ></input>
+      <br></br>
+      <button onClick={() => fetchNutritionalValue()}>Show nutritional value</button>
+      <button
+        onClick={() => {
+          setHideResult(true);
+          handleClear();
+        }}
+      >
+        Clear
+      </button>
+      <br />
+      {
+        nutritionalValue.length !== 0 ? 
+      (<div className="mealTable" hidden={hideResult}>
+        <TableComponent dataArray={nutritionalValue} />
+      </div>) : "" 
+      }
+    </div>
+  );
 }
 
 export default Meals;
