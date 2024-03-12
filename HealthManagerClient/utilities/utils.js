4d33f93490707bc95f1915649904baabfeaae8ab
Emil9411 <almadi.emil94@gmail.com>
2024-03-12 16:24:51 +0100

    Method extended to make it useful in more cases.

--- HealthManagerClient/utilities/utils.js
+++ HealthManagerClient/utilities/utils.js
@@ -1,9 +1,18 @@
-export function capitalizeWords(str) {
-    if (typeof str !== 'string') return '';
-    if (str.length === 0) return '';
-    if (!str) return '';
-    return str.replace(/\b\w/g, function(match) {
-      return match.toUpperCase();
-    });
+export function capitalizeWords(input) {
+  if (typeof input === 'number') {
+    input = String(input); // Convert numbers to strings
   }
-  
+
+  if (typeof input !== 'string') return '';
+  if (input.length === 0) return '';
+
+  return input.replace(/\b\w|_/g, function(match) {
+    if (match === '_') {
+      return ' '; // Replace underscores with whitespace
+    } else {
+      return match.toUpperCase();
+    }
+  }).replace(/(g|mg)$/, function(match) {
+    return match.toLowerCase(); // Keep "g" or "mg" in lowercase
+  });
+}
