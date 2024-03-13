437de131baef7ceed9667e770bb4704fffd25cc4
Emil9411 <almadi.emil94@gmail.com>
2024-03-13 17:11:24 +0100

    Logout button created.
    Co-Authored-By: Takács Tímea Erika <Tyna1992@users.noreply.github.com>
    Co-Authored-By: szoszo23 <szoszo23@users.noreply.github.com>

---
+++ HealthManagerClient/src/Components/LogoutButton.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+
+function LogoutButton() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  async function handleLogout() {
+    try {
+      const response = await fetch("/api/Auth/Logout", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        if (location.pathname !== "/") {
+          navigate("/");
+          window.location.reload();
+        } else {
+          window.location.reload();
+        }
+      }
+    } catch (error) {
+      console.error("Logout failed", error);
+    }
+  }
+
+  return <button onClick={handleLogout}>Logout</button>;
+}
+
+export default LogoutButton;
