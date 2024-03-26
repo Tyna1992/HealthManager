f8bf01739c267ba4a6bed5cae1533f5ea0cc0268
Emil9411 <almadi.emil94@gmail.com>
2024-03-26 15:40:31 +0100

    ActivitiesControllerTest implemented
    Co-Authored-By: Takács Tímea Erika <132925519+Tyna1992@users.noreply.github.com>
    Co-Authored-By: szoszo23 <74458415+szoszo23@users.noreply.github.com>

---
+++ HealthManagerServer/HealthManagerTest/ActivitiesControllerTest.cs
@@ -0,0 +1,36 @@
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using HealthManagerServer.Model;
+using Xunit.Abstractions;
+
+namespace HealthManagerTest;
+
+public class ActivitiesControllerTest
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+    
+    public ActivitiesControllerTest(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+    
+    [Fact]
+    public async Task GetActivityData_NoAuthentication_Test()
+    {
+        var app = new HealthManagerWebApplicationFactory();
+        var client = app.CreateClient();
+        var activityName = "running";
+        var weight = 160;
+        var duration = 60;
+        
+        var response = await client.GetAsync($"/api/activities/{activityName}/{weight}/{duration}");
+        _testOutputHelper.WriteLine(response.ToString());
+        
+        response.EnsureSuccessStatusCode();
+        var activityData = await response.Content.ReadFromJsonAsync<Activity>();
+        
+        Assert.NotNull(activityData);
+        Assert.Equal("Running, 5 mph (12 minute mile)", activityData.Name);
+    }
+    
+}
