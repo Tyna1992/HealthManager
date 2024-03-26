4ba29dce277ca66113f6e754d2f84a362edfde87
Emil9411 <almadi.emil94@gmail.com>
2024-03-26 15:38:16 +0100

    Added DotNetEnv to the test project
    Co-Authored-By: Takács Tímea Erika <132925519+Tyna1992@users.noreply.github.com>
    Co-Authored-By: szoszo23 <74458415+szoszo23@users.noreply.github.com>

--- HealthManagerServer/HealthManagerTest/HealthManagerWebApplicationFactory.cs
+++ HealthManagerServer/HealthManagerTest/HealthManagerWebApplicationFactory.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace HealthManagerTest;
 
@@ -18,8 +17,8 @@ internal class HealthManagerWebApplicationFactory : WebApplicationFactory<Progra
             services.RemoveAll(typeof(DbContextOptions<DataBaseContext>));
             services.RemoveAll(typeof(DbContextOptions<UserContext>));
 
-            var connectionString =
-                "Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=HealthManagerTest;Data Source=TIMI\\SQLEXPRESS;TrustServerCertificate=true;";
+            var connectionString = GetConnectionString();
+            
             services.AddDbContext<DataBaseContext>(options =>
                 options.UseSqlServer(connectionString));
             
@@ -50,8 +49,9 @@ internal class HealthManagerWebApplicationFactory : WebApplicationFactory<Progra
     
     private static string? GetConnectionString()
     {
-        DotNetEnv.Env.Load();
+        DotNetEnv.Env.Load(@"C:\Users\User\Desktop\HealthManagerProject\HealthManager\HealthManagerServer\HealthManagerTest\.env");
         var connectionString = DotNetEnv.Env.GetString("CONNECTION_STRING_TEST");
+        Console.WriteLine(connectionString);
         return connectionString;
     }
 }
