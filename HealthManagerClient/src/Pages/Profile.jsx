c23855299638fe2e2eed7282651afad089d8fb09
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-03-27 19:00:01 +0100

    Basic profile page created

--- HealthManagerClient/src/Pages/Profile.jsx
+++ HealthManagerClient/src/Pages/Profile.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useLocation} from "react-router-dom";
 
 function Profile() {
+
+    const [user, setUser] = useState(null);
+    const [email, setEmail] = useState(null);
+    
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch("/api/Auth/WhoAmI", {
+                    method: "GET",
+                    credentials: "include",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                const data = await response.json();
+                if (data) {
+                    setUser(data.userName);
+                    setEmail(data.email);
+                }
+            } catch (error) {
+                console.log("Error", error);
+            }
+        }
+        fetchData();
+    }, []);
+    
     return (
-        <div>
+        <div className="profile">
             <h1>User Profile</h1>
-            <h3>In development...</h3>
+            <br/>
+            <h2>Username:</h2>
+            <h3>{user}</h3>
+            <br/>
+            <h2>Email:</h2>
+            
+            <h3>{email}</h3>
+
+
         </div>
     );
 };
