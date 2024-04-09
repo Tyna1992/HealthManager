1c93cbdb078681a90937ddf7b10a13ba8881b2e0
Emil9411 <almadi.emil94@gmail.com>
2024-04-10 00:48:19 +0200

    AdminTableComponent now checks the dataArray.
    Checks if its empty, or not null, or if its an array at all.

--- HealthManagerClient/src/Components/AdminTableComponent.jsx
+++ HealthManagerClient/src/Components/AdminTableComponent.jsx
@@ -1,48 +1,46 @@
 import React, { useEffect, useState } from "react";
 import "../index.css";
-import {capitalizeWords} from "../../utilities/utils";
+import { capitalizeWords } from "../../utilities/utils";
 
-function AdminTableComponent({dataArray, onDelete, onEdit}) {
-    const [dataKeys, setDataKeys] = useState([]);
+function AdminTableComponent({ dataArray, onDelete, onEdit }) {
+  const [dataKeys, setDataKeys] = useState([]);
 
-    useEffect(() => {
-        if (dataArray.length > 0) {
-          setDataKeys(Object.keys(dataArray[0]));
-        }
-      }, [dataArray]);
+  useEffect(() => {
+    if (dataArray.length > 0) {
+      setDataKeys(Object.keys(dataArray[0]));
+    }
+  }, [dataArray]);
 
-    return (
-        <div>
-            {dataArray.length === 0 ? (
-                <h1>No data to display</h1>
-            ) : (
-                <div className="table">
-                    <table>
-                        <tbody>
-                            <tr>
-                                {dataKeys.map((dataKey, index) => (
-                                    <th key={index}>{capitalizeWords(dataKey)}</th>
-                                ))}
-                            </tr>
-                            {dataArray.map((data) => (
-                                <tr key={data.id}>
-                                    {dataKeys.map((dataKey, index) => (
-                                        <td key={dataKey + index}>{capitalizeWords(data[dataKey])}</td>
-                                    ))}
-                                    <td>
-                                        <button onClick={() => onDelete(data.id)}>Delete</button>
-                                    </td>
-                                    <td>
-                                        <button onClick={() => onEdit(data)}>Edit</button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            )}
-        </div>
-    );
+  if (!dataArray || !Array.isArray(dataArray) || dataArray.length === 0) {
+    return <h1>No data to display</h1>;
+  }
+
+  return (
+    <div className="table">
+      <table>
+        <tbody>
+          <tr>
+            {dataKeys.map((dataKey, index) => (
+              <th key={index}>{capitalizeWords(dataKey)}</th>
+            ))}
+          </tr>
+          {dataArray.map((data) => (
+            <tr key={data.id}>
+              {dataKeys.map((dataKey, index) => (
+                <td key={dataKey + index}>{capitalizeWords(data[dataKey])}</td>
+              ))}
+              <td>
+                <button onClick={() => onDelete(data.id)}>Delete</button>
+              </td>
+              <td>
+                <button onClick={() => onEdit(data)}>Edit</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default AdminTableComponent;
