b8948e74d347740d2dd0c43e9f8b3cc9612cd281
Emil9411 <almadi.emil94@gmail.com>
2024-04-09 16:54:03 +0200

    Breakpoints added.
    Co-Authored-By: szoszo23 <74458415+szoszo23@users.noreply.github.com>
    Co-Authored-By: Takács Tímea Erika <132925519+Tyna1992@users.noreply.github.com>

--- HealthManagerClient/src/Components/Forms/MealUpdateForm.jsx
+++ HealthManagerClient/src/Components/Forms/MealUpdateForm.jsx
@@ -32,42 +32,55 @@ function MealDataUpdateForm({data, onEdit, clearData}){
             <h1>Update Meal Data</h1>
             <br />
             <label>Id:</label>
-            <input type="text" name="id" value={formData.id} disabled={true} />
+            <br />
+            <input type="number" name="id" value={formData.id} disabled={true} />
             <br />
             <label>Name:</label>
+            <br />
             <input type="text" name="name" value={formData.name} onChange={handleChange} />
             <br />
             <label>Serving Size (g):</label>
+            <br />
             <input type="text" name="servingSize" value={formData.servingSize} onChange={handleChange} />
             <br />
             <label>Calories:</label>
+            <br />
             <input type="text" name="calories" value={formData.calories} onChange={handleChange} />
             <br />
             <label>Total Fat (g):</label>
+            <br />
             <input type="text" name="totalFat" value={formData.totalFat} onChange={handleChange} />
             <br />
             <label>Saturated Fat (g):</label>
+            <br />
             <input type="text" name="saturatedFat" value={formData.saturatedFat} onChange={handleChange} />
             <br />
             <label>Protein (g):</label>
+            <br />
             <input type="text" name="protein" value={formData.protein} onChange={handleChange} />
             <br />
             <label>Sodium (mg):</label>
+            <br />
             <input type="text" name="sodium" value={formData.sodium} onChange={handleChange} />
             <br />
             <label>Potassium (mg):</label>
+            <br />
             <input type="text" name="potassium" value={formData.potassium} onChange={handleChange} />
             <br />
             <label>Cholesterol (mg):</label>
+            <br />
             <input type="text" name="cholesterol" value={formData.cholesterol} onChange={handleChange} />
             <br />
             <label>Carbohydrates (g):</label>
+            <br />
             <input type="text" name="carbohydrates" value={formData.carbohydrates} onChange={handleChange} />
             <br />
             <label>Fiber (g):</label>
+            <br />
             <input type="text" name="fiber" value={formData.fiber} onChange={handleChange} />
             <br />
             <label>Sugar (g):</label>
+            <br />
             <input type="text" name="sugar" value={formData.sugar} onChange={handleChange} />
             <br />
             <button onClick={handleUpdateClick}>Update</button>
