0b95b56da977c0a4a9423682a8c3cda57b04c79f
Emil9411 <almadi.emil94@gmail.com>
2024-04-10 10:06:45 +0200

    Updated databasecontext with mealplans table.
    Co-Authored-By: Takács Tímea Erika <132925519+Tyna1992@users.noreply.github.com>

--- HealthManagerServer/HealthManagerServer/Data/DataBaseContext.cs
+++ HealthManagerServer/HealthManagerServer/Data/DataBaseContext.cs
@@ -9,23 +9,31 @@ public class DataBaseContext : DbContext
     public DbSet<Nutrition> Nutritions { get; set; }
     public DbSet<Activity> Activities { get; set; }
     public DbSet<Cocktail> Cocktails { get; set; }
+    public DbSet<MealPlan> MealPlans { get; set; }
 
     public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
     {
-       
+
     }
 
 
-    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    // {
-    //     var connectionString = DotNetEnv.Env.GetString("CONNECTION_STRING");
-    //     optionsBuilder.UseSqlServer(connectionString);
-    // }
+    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Activity>().HasIndex(a => new { a.Name, a.Duration_minutes, a.Weight }).IsUnique();
         modelBuilder.Entity<Nutrition>().HasIndex(n => new { n.Name, n.Serving_size_g }).IsUnique();
         modelBuilder.Entity<Cocktail>().HasIndex(c => c.Name).IsUnique();
+        // meal plan unnique index, user id is foreign key to user, meal id is foreign key to meal
+        modelBuilder.Entity<MealPlan>()
+        .HasOne(mp => mp.User)
+        .WithMany() // Assuming a User can have multiple MealPlans
+        .HasForeignKey(mp => mp.UserId);
+
+    modelBuilder.Entity<MealPlan>()
+        .HasOne(mp => mp.Meal)
+        .WithMany() // Assuming a Meal can be in multiple MealPlans
+        .HasForeignKey(mp => mp.MealId);
+
     }
 }
