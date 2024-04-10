5ed3652c4f84aaed97b3cb085cd0e243973885bd
Emil9411 <almadi.emil94@gmail.com>
2024-04-10 10:06:02 +0200

    MealPlan model created.
    Co-Authored-By: Takács Tímea Erika <132925519+Tyna1992@users.noreply.github.com>

---
+++ HealthManagerServer/HealthManagerServer/Model/MealPlan.cs
@@ -0,0 +1,13 @@
+namespace HealthManagerServer.Model;
+
+public class MealPlan
+{
+    public Guid Id { get; set; }
+    public string? UserId { get; set; }
+    public ApplicationUser? User { get; set; }
+    public int MealId { get; set; }
+    public Nutrition? Meal { get; set; }
+    public DateTime Date { get; set; }
+    public string? MealTime { get; set; }
+    public string? DayOfTheWeek => Date.DayOfWeek.ToString("dddd");
+}
