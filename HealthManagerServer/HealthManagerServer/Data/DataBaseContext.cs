bacca8c4e0b222d5e6714ac6cd1b06cafc88b283
Emil9411 <almadi.emil94@gmail.com>
2024-04-10 16:04:09 +0200

    Database context modified.
    Co-Authored-By: szoszo23 <74458415+szoszo23@users.noreply.github.com>
    Co-Authored-By: Takács Tímea Erika <132925519+Tyna1992@users.noreply.github.com>

--- HealthManagerServer/HealthManagerServer/Data/DataBaseContext.cs
+++ HealthManagerServer/HealthManagerServer/Data/DataBaseContext.cs
@@ -24,16 +24,14 @@ public class DataBaseContext : DbContext
         modelBuilder.Entity<Activity>().HasIndex(a => new { a.Name, a.Duration_minutes, a.Weight }).IsUnique();
         modelBuilder.Entity<Nutrition>().HasIndex(n => new { n.Name, n.Serving_size_g }).IsUnique();
         modelBuilder.Entity<Cocktail>().HasIndex(c => c.Name).IsUnique();
-        // meal plan unnique index, user id is foreign key to user, meal id is foreign key to meal
-        modelBuilder.Entity<MealPlan>()
-        .HasOne(mp => mp.User)
-        .WithMany() // Assuming a User can have multiple MealPlans
-        .HasForeignKey(mp => mp.UserId);
 
-    modelBuilder.Entity<MealPlan>()
+        modelBuilder.Entity<MealPlan>()
         .HasOne(mp => mp.Meal)
         .WithMany() // Assuming a Meal can be in multiple MealPlans
-        .HasForeignKey(mp => mp.MealId);
+        .HasForeignKey(mp => mp.MealId)
+        .IsRequired();
+
+        base.OnModelCreating(modelBuilder);
 
     }
 }
