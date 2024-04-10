82216b119e2b0d92ba28b10975d78d53c1c15589
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-04-10 22:17:19 +0200

    Integration tests are created to test UserControllers endpoints

---
+++ HealthManagerServer/HealthManagerTest/UserControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using HealthManagerServer.Service;
+
+namespace HealthManagerTest;
+
+
+[Collection("Integration")]
+public class UserControllerTests
+{
+    [Fact]
+    public async Task GetAllUsers_AdminAuthorized()
+    {
+        var app = new HealthManagerWebApplicationFactory();
+        var client = app.CreateClient();
+        
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("TestAdminScheme");
+        var response = await client.GetAsync($"/api/user/getAll");
+        response.EnsureSuccessStatusCode();
+        
+        var responseString = await response.Content.ReadAsStringAsync();
+        var users = await response.Content.ReadFromJsonAsync<List<UserResponse>>();
+        
+        Assert.NotNull(users);
+        Assert.Single(users);
+    }
+    
+    [Fact]
+    public async Task GetUserByEmail_AdminAuthorized()
+    {
+        var app = new HealthManagerWebApplicationFactory();
+        var client = app.CreateClient();
+        
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("TestAdminScheme");
+        var email = "admin@admin.com";
+        var response = await client.GetAsync($"/api/user/getByEmail/{email}");
+        response.EnsureSuccessStatusCode();
+        
+        var responseString = await response.Content.ReadAsStringAsync();
+        var user = await response.Content.ReadFromJsonAsync<UserResponse>();
+        
+        Assert.NotNull(user);
+        Assert.Equal(email, user.Email);
+        
+    }
+}
