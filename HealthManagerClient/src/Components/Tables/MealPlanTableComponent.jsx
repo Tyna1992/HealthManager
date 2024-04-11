b2cdc51981446b485b3e13cc13f676f597f92892
Emil9411 <almadi.emil94@gmail.com>
2024-04-11 14:45:11 +0200

    MealPlanTableComponent reworked, unnecessary useEffect, useState removed.
    Co-Authored-By: szoszo23 <74458415+szoszo23@users.noreply.github.com>
    Co-Authored-By: Takács Tímea Erika <132925519+Tyna1992@users.noreply.github.com>

--- HealthManagerClient/src/Components/Tables/MealPlanTableComponent.jsx
+++ HealthManagerClient/src/Components/Tables/MealPlanTableComponent.jsx
@@ -1,45 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../../index.css";
 import { capitalizeWords } from "../../../utilities/utils";
 
-
-function MealPlanTableComponent({dataArray}){
-    const [dataKeys, setDataKeys] = useState([]);
-
-    useEffect(() => {
-        console.log(dataArray);
-        let columns = Object.keys(dataArray[0]);
-        let filteredColumns = columns.filter((column) => column !== "id" && column !== "mealId" && column !== "userName");
-        setDataKeys(filteredColumns);
-    }, []);
-
-
-    return (
-        <div className="table">
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Meal name</th>
-                        <th>Serving size</th>
-                        <th>Calories</th>
-                        <th>Date</th>
-                        <th>Meal time</th>
-                        <th>Day</th>
-                    </tr>
-                    {dataArray.map((data) => (
-                        <tr key={data.id}>
-                            <td>{capitalizeWords(data.meal.name)}</td>
-                            <td>{data.meal.serving_size_g} g</td>
-                            <td>{data.meal.calories} cal</td>
-                            <td>{data.date.split("T")[0]}</td>
-                            <td>{capitalizeWords(data.mealTime)}</td>
-                            <td>{data.dayOfTheWeek}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    )
+function MealPlanTableComponent({ dataArray, onDelete, onEdit }) {
+  return (
+    <div className="table">
+      <table>
+        <tbody>
+          <tr>
+            <th>Meal name</th>
+            <th>Serving size</th>
+            <th>Calories</th>
+            <th>Date</th>
+            <th>Meal time</th>
+            <th>Day</th>
+            <th>Delete</th>
+            <th>Edit</th>
+          </tr>
+          {dataArray.map((data) => (
+            <tr key={data.id}>
+              <td>{capitalizeWords(data.meal.name)}</td>
+              <td>{data.meal.serving_size_g} g</td>
+              <td>{data.meal.calories} cal</td>
+              <td>{data.date.split("T")[0]}</td>
+              <td>{capitalizeWords(data.mealTime)}</td>
+              <td>{data.dayOfTheWeek}</td>
+              <td>
+                <button onClick={() => onDelete(data.id)}>Delete</button>
+              </td>
+              <td>
+                <button onClick={() => onEdit(data)} disabled>
+                  Edit
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default MealPlanTableComponent;
